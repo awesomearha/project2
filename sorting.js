936062af7d0341defac68e72bc3870ac3c39b618
awesomearha <138312704+awesomearha@users.noreply.github.com>
2023-12-08 15:44:41 +0400

    Add files via upload

---
+++ sorting.js
@@ -0,0 +1,8 @@
+function sorting() {
+	var names_of_people, sort; (); // add the sort function here
+
+	// .sort()
+	var i = names_of_people.join("<br>");
+	console.log(names_of_people);
+	document.getElementById("sorted").innerHTML = i.toString();
+}
